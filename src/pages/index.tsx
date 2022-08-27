38ae01cb30ea9ce5d9ceec7dd6b05b4d2e50a0dc
baijanathTharu <bnchaudhary.tharu@gmail.com>
2022-08-27 16:19:40 +0545

    :bug: remove pnpm and use yarn and setup tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div className='mx-auto max-w-5xl min-w-[450px]'>
+      <h1 className='text-red-400'>Next Auth</h1>
+    </div>
+  );
+};
+
+export default Home;
